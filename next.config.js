7867ffff27b23f896805d5f2176287eefe9e8bc5
Jeff Reiner <theundeadghost@gmail.com>
2024-09-19 16:44:07 +0200

    Adding in Plausible

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+const { withPlausibleProxy } = require("next-plausible");
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra({
-  reactStrictMode: true,
-});
+module.exports = withPlausibleProxy()(withNextra({ reactStrictMode: true }));
